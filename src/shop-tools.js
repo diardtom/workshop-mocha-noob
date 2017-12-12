8547cf104140a1026590af6ab081de21a65f087f
diardtom <tomdiard@yahoo.fr>
2017-12-12 15:27:59 +0100

    exercice 1

---
+++ src/shop-tools.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expected;
+const products = [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato' ];
+
+exports.getPrices=function (){
+  return {"banana": 1, "potato": 2, "tomato": 3, "cucumber": 4,"salad": 5, "apple": 6};
+
+}
