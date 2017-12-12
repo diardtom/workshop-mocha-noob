e8f532eaf25570d5810cebbd96a84c5b8c30c7fe
diardtom <tomdiard@yahoo.fr>
2017-12-12 15:20:57 +0100

    Correction exercice 1

---
+++ test/shop-tools.test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+var getPrices = require('../src/shop-tools').getPrices;
+
+describe('Function getPrices()', function() {
+
+
+it('expect return the good result', function() {
+expect(getPrices()).to.eql({banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6});
+})
+});
